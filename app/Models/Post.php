4782bfe814dc70a09cb9152a0651ea5e693683ca
Kenneth <kenneth.muuli@gmail.com>
2023-05-22 21:46:33 +0300

    Episode 13

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,16 +22,21 @@ public $slug;
         $this -> slug = $slug;
     }
 
-    public static function all(){
-        return collect(File::files(resource_path("posts")))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($document)=>new Post(
-                $document -> title,
-                $document -> excerpt,
-                $document -> date,
-                $document -> body(),
-                $document -> slug
-        ));
+    public static function all()
+    {
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document)=>new Post(
+                    $document -> title,
+                    $document -> excerpt,
+                    $document -> date,
+                    $document -> body(),
+                    $document -> slug
+            ))
+            ->sortByDesc('date');
+        });
+        
     }
     
     public static function find ($slug) 
