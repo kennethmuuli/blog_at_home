6f4f19debdf3a3f1a7f0fe5ceda67c8cde78b620
Kenneth <kenneth.muuli@gmail.com>
2023-05-22 22:13:52 +0300

    Episode 15

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
+<x-layout>
+    @foreach($posts as $post)
+        <article class="{{$loop->even ? 'foobar': ''}}">
+            
+            <h1>
+                <a href="/posts/{{ $post -> slug }}">
+                    {{$post -> title}}
+                </a>
+            </h1>
 
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-
-    <body>
-        @foreach($posts as $post)
-            <article class="{{$loop->even ? 'foobar': ''}}">
-                
-                <h1>
-                    <a href="/posts/{{ $post -> slug }}">
-                        {{$post -> title}}
-                    </a>
-                </h1>
-
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
-    </body>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
