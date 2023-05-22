0994e7ef9fe8ca1c6c2a15727e4e20b8646fa933
Kenneth <kenneth.muuli@gmail.com>
2023-05-22 22:01:22 +0300

    Episode 14

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,18 +4,18 @@
     <link rel="stylesheet" href="/app.css">
 
     <body>
-        <?php foreach($posts as $post) : ?>
-        <article>
-            
-            <h1>
-                <a href="/posts/<?= $post -> slug; ?>">
-                    <?= $post->title; ?>
-                </a>
-            </h1>
+        @foreach($posts as $post)
+            <article class="{{$loop->even ? 'foobar': ''}}">
+                
+                <h1>
+                    <a href="/posts/{{ $post -> slug }}">
+                        {{$post -> title}}
+                    </a>
+                </h1>
 
-            <div>
-                <?= $post->excerpt; ?>
-            </div>
-        </article>
-        <?php endforeach; ?>
+                <div>
+                    {{ $post->excerpt }}
+                </div>
+            </article>
+        @endforeach
     </body>
