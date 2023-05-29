165aa36347d5c60ab668984251b4a05a3ec81dc9
Kenneth <kenneth.muuli@gmail.com>
2023-05-29 21:11:30 +0300

    Episode 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,56 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-
-        $user = User::factory()->create();
-
-        $family = Category::create([
-            'name'=>'Family',
-            'slug'=>'family'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $personal = Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        
-        $work = Category::create([
-            'name'=>'Work',
-            'slug'=>'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id'=> $family->id,
-            'title'=>'My Family Post',
-            'slug'=>'my-first-post',
-            'excerpt'=>'Lorem ipsum dolar sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit nulla cum molestiae repellat quibusdam atque culpa quis asperiores et eaque. Architecto tempora illo animi molestias dolorum harum commodi error numquam!</p>'
-
-            
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id'=>$work->id,
-            'title'=>'My Work Post',
-            'slug'=>'my-work-post',
-            'excerpt'=>'Lorem ipsum dolar sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit nulla cum molestiae repellat quibusdam atque culpa quis asperiores et eaque. Architecto tempora illo animi molestias dolorum harum commodi error numquam!</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id'=>$personal->id,
-            'title'=>'My Personal Post',
-            'slug'=>'my-personal-post',
-            'excerpt'=>'Lorem ipsum dolar sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit nulla cum molestiae repellat quibusdam atque culpa quis asperiores et eaque. Architecto tempora illo animi molestias dolorum harum commodi error numquam!</p>'
-        ]);
     }
 }
