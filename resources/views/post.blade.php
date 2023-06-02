fb61ad5713063051d656286474b89111ca6bff06
Kenneth <kenneth.muuli@gmail.com>
2023-06-02 09:22:35 +0300

    Episode 31

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    
+
     <article>
         <h1>
             {!! $post->title !!}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,27 @@
 
 @section('content')
 
-    @foreach($posts as $post)
+@include('_posts-header')
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card />
+        <x-post-card />  
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card />
+        <x-post-card />   
+        <x-post-card />       
+
+        
+    </div>
+</main>
+
+{{-- USED TO BE --}}
+    {{-- @foreach($posts as $post)
         <article>
             
             <h1>
@@ -19,6 +39,6 @@
                 {{ $post->excerpt }}
             </div>
         </article>
-    @endforeach
+    @endforeach --}}
     
 @endsection
