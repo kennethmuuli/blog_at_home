ca956af863d970f32fe4e13b45bbc71d76a4bd9a
Kenneth <kenneth.muuli@gmail.com>
2023-06-10 18:46:04 +0300

    Episode 50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required',
+            'password' => 'required'
+
+        ]);
+
+        if (auth()->attempt($attributes))
+        {
+            session()->regenerate();
+            // Above against sessions fixation attacks
+            return redirect('/')->with('success', 'Logged In');
+        }
+
+        // Method 2
+        throw ValidationException::withMessages(['email' => 'Your provided ceredentials could not be verified.']);
+
+        // //  Method 1
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Your provided ceredentials could not be verified.']);
+    }
+
     public function destroy()
     {
         auth()->logout();
