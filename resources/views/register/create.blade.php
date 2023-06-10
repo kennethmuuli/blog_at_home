bae7a299920a62d7643a82959905b334484cb4e8
Kenneth <kenneth.muuli@gmail.com>
2023-06-10 10:43:54 +0300

    Episode 47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -20,8 +20,13 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name')}}"
                         required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 <div class="mb-6 mt-5">
                     <label
@@ -35,8 +40,13 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username')}}"
                         required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 <div class="mb-6 mt-5">
                     <label
@@ -50,8 +60,13 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{ old('email')}}"
                         required
                     >
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
 
@@ -70,6 +85,10 @@
                         required
                     >
 
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 <div class="mb-6 mt-5">
                     <button
                         type="submit"
@@ -78,6 +97,15 @@
                         Submit
                     </button>
                 </div>
+
+                {{-- Display all errors method --}}
+                @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li class="text-red-500 text-xs">{{$error}}</li>
+                        @endforeach
+                    </ul>
+                @endif
 
             </form>
         </main>
