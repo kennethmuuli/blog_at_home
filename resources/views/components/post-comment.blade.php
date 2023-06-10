cde964ceab0cf925f0a932bcf82744bba634fd6c
Kenneth <kenneth.muuli@gmail.com>
2023-06-10 21:24:43 +0300

    Episode 54

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,23 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/60" alt=""
+        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{$comment->id}}" alt=""
         width="60" height="60">
     </div>
 
     <div>
         <header class="mb-4">
             <h3 class="font-bold">
-                John Doe
+                {{$comment->author->username}}
             </h3>
 
             <p class="text-xs">
-                Posted<time> 8 monthsago</time>
+                Posted<time> {{$comment->created_at}}</time>
             </p>
 
             <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam nemo ducimus quasi necessitatibus sunt minima rem ut vero voluptates laudantium!
+                {{$comment->body}}
             </p>
         </header>
     </div>
