9b0dcd80ba6aca307e183de6ec33b6d18fdba5d7
Kenneth <kenneth.muuli@gmail.com>
2023-06-10 10:59:46 +0300

    Episode 48

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -98,15 +98,6 @@
                     </button>
                 </div>
 
-                {{-- Display all errors method --}}
-                @if ($errors->any())
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li class="text-red-500 text-xs">{{$error}}</li>
-                        @endforeach
-                    </ul>
-                @endif
-
             </form>
         </main>
     </section>
