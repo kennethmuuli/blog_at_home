1345046884925714943b06e06c398e5d630dae2d
Kenneth <kenneth.muuli@gmail.com>
2023-07-04 13:53:18 +0300

    Episode 58

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function() {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('c6e8cbd116', [
+        'email_address' => 'kenneth.muuli@gmail.com',
+        'status'=> 'subscribed'
+    ]);
+    
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
