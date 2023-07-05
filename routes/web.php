a2c5aaad2f2786406bbabaea0440abed0d8b61b9
Kenneth <kenneth.muuli@gmail.com>
2023-07-05 16:51:17 +0300

    Episode 59

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('ping', function() {
+Route::post('newsletter', function() {
+    request()->validate(['email' => 'required|email']);
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -14,12 +16,18 @@ Route::get('ping', function() {
         'server' => 'us21'
     ]);
 
-    $response = $mailchimp->lists->addListMember('c6e8cbd116', [
-        'email_address' => 'kenneth.muuli@gmail.com',
-        'status'=> 'subscribed'
-    ]);
-    
-    dd($response);
+    try {
+        $response = $mailchimp->lists->addListMember('c6e8cbd116', [
+            'email_address' => request('email'),
+            'status'=> 'subscribed'
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+
+    return redirect('/')->with('success', 'You are now signed up for out newsletter!');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
