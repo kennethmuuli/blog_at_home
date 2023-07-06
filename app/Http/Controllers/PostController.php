13cf07b638c99e330e952ba2de915b9224aef9da
Kenneth <kenneth.muuli@gmail.com>
2023-07-06 09:29:56 +0300

    Episode 62

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
@@ -18,9 +19,16 @@ class PostController extends Controller
     }
 
     public function show(Post $post) {
-        return view ('posts.show',
+        return view('posts.show',
             [
                 'post' => $post
             ]);
+    }
+
+    public function create()
+    {
+        
+
+        return view('posts.create');
     }
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+
+    <x-slot name="content">
+        <section class="px-6 py-8">
+            Hello World!
+        </section>
+
+
+    </x-slot>
+</x-layout>
